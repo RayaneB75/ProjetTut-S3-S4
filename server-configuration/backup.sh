6bcffc21019e573d2ae293ab99ccb9f398c10eb3
RayaneB75 <rayou13004@gmail.com>
2021-12-22 22:21:55 +0100

    Enlevage du module en trop

--- server-configuration/backup.sh
+++ server-configuration/backup.sh
@@ -1,18 +1,12 @@
 #!/bin/bash
 
 #On fait un backup des containers:
-if [ $1 -eq "backup"]
-then
-    for i in www openvpn ns portainer
-    do
-        echo "Backup du container $i en cours"
-        sudo docker commit -p $i backup-$i
-        echo "Sauvegarde dans le répertoire /var/services/homes/rayane/backup"
-        sudo docker save -o /var/services/homes/rayane/backup/backup-$i.tar
-    done
-elif [ $1 -eq "load" ]
-then
-    echo "Où sont les container à récupérer"
-    read var
-    sudo docker load -i $var/*
-fi
+
+
+for i in www openvpn ns portainer
+do
+    echo "Backup du container $i en cours"
+    sudo docker commit -p $i backup-$i
+    echo "Sauvegarde dans le répertoire /var/services/homes/rayane/backup"
+    sudo docker save -o /var/services/homes/rayane/backup/backup-$i.tar
+done
