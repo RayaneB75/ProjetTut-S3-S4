637f01c6b99265b99dbc473e2ca6c08116c50679
RayaneB75 <rayane.belkhir@icloud.com>
2021-12-23 20:14:43 +0100

    Update deploy.sh
    Fait :
    - Update repository
    - Installation paquetages système
    - Installation paquetages docker
    
    À faire :
    - Ajout repository docker
    - Lancer les bonnes VM docker avec les bons paramètre (cf. docker-compose.yaml)

--- server-configuration/deploy.sh
+++ server-configuration/deploy.sh
@@ -1,33 +1,54 @@
 #!/bin/bash
-# Script pour déployer les VM sur un PC linux quelconque
-[[ $EUID -ne 0 ]] && echo "Le script doit être lancé en root: #sudo <script>" && exit 1
-echo "Lancement du script de déploiement"
+
+# Variables globales
+shopt -s expand_aliases
+alias echo="echo -e"
+rouge="\e[31m"
+rien="\e[0m"
+vert="\e[32m"
+ul="\e[4m"
+
+# On vérifie que le script est bien execute en root (sudo)
+[[ $EUID -ne 0 ]] && echo "Le script doit être lancé en$rouge root$rien:$vert \$sudo <script> $rien" && exit 1
+echo "$ul Lancement du script de déploiement $rien"
 
 # Si un répertoire "projettut" n'existe pas on le crée
-[[ ! -d projettut ]] && mkdir projettut && [[ $? -ne 0 ]] && echo "Vérifier les droits d'accès de ce répertoire pour l'utilisateur $USER" && exit 1
+[[ ! -d projettut ]] && mkdir projettut && [[ $? -ne 0 ]] && echo "$ul\Vérifier les droits d'accès de ce répertoire pour l'utilisateur$rouge $USER$rouge$ul" && exit 1
 chmod 777 projettut
 cd projettut # On rentre dans le répertoire
 
 # On met à jour les repositories
 echo "---------------------------------------------------------"
-echo -e "Récupération des paquetages nécessaires\n"
+echo "$ul Récupération des paquetages nécessaires $rien \n"
 echo "##########################################"
-echo "Mise à jour des repository :"
+echo "$ul Mise à jour des repository : $rien"
 sudo apt update
-[[ $? -ne 0 ]] && echo "##########################################" && echo "Erreur. Vérifier la connexion réseau" && exit 1
-echo -e "##########################################"
-echo -e "\nRepositories OK"
+[[ $? -ne 0 ]] && echo "##########################################" && echo "$rouge Erreur. $rien $ul Vérifier la connexion réseau $rien" && exit 1
+echo "##########################################"
+echo "\nRepositories$vert OK $rien"
 
-# Installation des paquetages 1 par 1
-for i in curl docker-ce docker-ce-cli software-properties-common containerd.io
-do
-    [[ $i -eq "containerd" ]] && 
-    echo -e "Installation du paquetage $i \n"
-    echo "##########################################"
-    sudo apt install $i -y
-    [[ $? -ne 0 ]] && echo "Erreur. Vérifier l'installation du paquetage $i" && continue
-    echo "##########################################"
-    echo -e "\nInstallation de $i OK"
-done
+# Function installation de paquetage 1 par 1
+function getApp() {
+    for i in $*; do
+        echo "##########################################"
+        echo "$ul Installation du paquetage $i\n $rien"
+        sudo apt install $i -y
+        [[ $? -ne 0 ]] && echo "$rouge Erreur. $rien $ul Vérifier l'installation du paquetage $i $rien" && continue
+        echo "\nInstallation de $i$vert OK $rien"
+        echo "##########################################\n"
+    done
+}
 
-echo "---------------------------------------------------------"
+# Installation des paquetages système (pour faire la suite)
+app="ca-certificates curl gnupg lsb-release"
+getApp $app
+
+# Récupération du repository docker
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Installation des paquetages pour la maquette
+app="docker-ce docker-ce-cli containerd.io"
+getApp $app
