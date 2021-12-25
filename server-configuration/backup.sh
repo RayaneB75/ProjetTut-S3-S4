b34107c8cb38dfaf4519e6878e472873d8ca58cc
RayaneB75 <rayane.belkhir@icloud.com>
2021-12-25 13:19:00 +0100

    Deployment et backup des container

--- server-configuration/backup.sh
+++ server-configuration/backup.sh
@@ -6,5 +6,5 @@ do
 	echo "Backup du container $i en cours"
 	sudo docker commit -p $i backup-$i
 	echo "Sauvegarde dans le répertoire /var/services/homes/rayane/backup"
-	sudo docker save -o /var/services/homes/rayane/backup/backup-$i.tar
+	sudo docker save -o /var/services/homes/rayane/backup/backup-$i.tar backup-$i
 done
--- server-configuration/deploy.sh
+++ server-configuration/deploy.sh
@@ -1,54 +1,81 @@
 #!/bin/bash
 
 # Variables globales
-shopt -s expand_aliases
+shopt -s expand_aliases # Permet d'intégrer les alias dans les scripts
 alias echo="echo -e"
 rouge="\e[31m"
 rien="\e[0m"
 vert="\e[32m"
 ul="\e[4m"
 
-# On vérifie que le script est bien execute en root (sudo)
-[[ $EUID -ne 0 ]] && echo "Le script doit être lancé en$rouge root$rien:$vert \$sudo <script> $rien" && exit 1
-echo "$ul Lancement du script de déploiement $rien"
+# Function installation de paquetage 1 par 1
+function getApp() {
+	echo "##########################################"
+	echo "$ul Installation du paquetage $1 $rien \n"
+	sudo apt install $1 -y
+	[[ $? -ne 0 ]] && echo "$rouge Erreur. $rien $ul Vérifier l'installation du paquetage $1 $rien"
+	echo "\nInstallation de $1$vert OK $rien"
+	echo "##########################################\n"
+	done
+}
 
-# Si un répertoire "projettut" n'existe pas on le crée
+# Function de récupération des container backupé
+function getBack() {
+	echo "$ul Répertoire de sauvegarde des container $rien"
+	read rep
+	echo "##########################################"
+	echo "$ul Récupération du container $1 $rien\n "
+	docker load -i $rep/backup-$1
+	echo "##########################################"
+}
+
+# On vérifie que le script est bien execute en root (sudo)
+function getAdmin() {
+	[[ $EUID -ne 0 ]] && echo "Le script doit être lancé en$rouge root$rien:$vert \$sudo <script> $rien" && exit 1
+	echo "$ul Lancement du script de déploiement $rien"
+}
+
+# Récupération du repository docker
+function getRepo {
+	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+	echo \
+		"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+		$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+}
+
+# Mise à jour repositories
+function updateRepo {
+	echo "---------------------------------------------------------"
+	echo "$ul Récupération des paquetages nécessaires $rien \n"
+	echo "##########################################"
+	echo "$ul Mise à jour des repository : $rien"
+	sudo apt update
+	[[ $? -ne 0 ]] && echo "##########################################" && echo "$rouge Erreur. $rien $ul Vérifier la connexion réseau $rien" && exit 1
+	echo "##########################################"
+	echo "\nRepositories$vert OK $rien"
+}
+
+
+
+
+
+# ----------- main ----------- #
+
 [[ ! -d projettut ]] && mkdir projettut && [[ $? -ne 0 ]] && echo "$ul\Vérifier les droits d'accès de ce répertoire pour l'utilisateur$rouge $USER$rouge$ul" && exit 1
 chmod 777 projettut
 cd projettut # On rentre dans le répertoire
 
-# On met à jour les repositories
-echo "---------------------------------------------------------"
-echo "$ul Récupération des paquetages nécessaires $rien \n"
-echo "##########################################"
-echo "$ul Mise à jour des repository : $rien"
-sudo apt update
-[[ $? -ne 0 ]] && echo "##########################################" && echo "$rouge Erreur. $rien $ul Vérifier la connexion réseau $rien" && exit 1
-echo "##########################################"
-echo "\nRepositories$vert OK $rien"
-
-# Function installation de paquetage 1 par 1
-function getApp() {
-	for i in $*; do
-		echo "##########################################"
-		echo "$ul Installation du paquetage $i\n $rien"
-		sudo apt install $i -y
-		[[ $? -ne 0 ]] && echo "$rouge Erreur. $rien $ul Vérifier l'installation du paquetage $i $rien" && continue
-		echo "\nInstallation de $i$vert OK $rien"
-		echo "##########################################\n"
-	done
-}
-
-# Installation des paquetages système (pour faire la suite)
-app="ca-certificates curl gnupg lsb-release"
-getApp $app
-
-# Récupération du repository docker
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-	"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-	$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
+# On vérifie qu'on est bien en root
+getAdmin
+# Mise à jour des repositories
+updateRepo
+# Installation des paquetages système (pour rajouter les repositories)
+getApp "ca-certificates" ; getApp 'curl' ; getApp 'gnupg lsb-release'
+# On récupère les repositories nécessaires
+getRepo
+# Mise à jour des nouveaux repositories
+updateRepo
 # Installation des paquetages pour la maquette
-app="docker-ce docker-ce-cli containerd.io"
-getApp $app
+getApp "docker-ce" ; getApp "docker-ce-cli" ; getApp "containerd.io"
+# Récupération des container
+getBack "ns" ; getBack "www" ; getBack "portainer" ; getBack "openvpn"
