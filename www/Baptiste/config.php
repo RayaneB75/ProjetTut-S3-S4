47e9f0ff6edf32a7b7c0df7ae661bd7218bd6b6b
RayaneB75 <rayane.belkhir@icloud.com>
2022-02-28 14:28:58 +0100

    Update config.php

--- www/Baptiste/config.php
+++ www/Baptiste/config.php
@@ -1,9 +1,9 @@
 <?php
 	// Informations d'identification
 	define('DB_SERVER', 'localhost');
-	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
-	//define('DB_USERNAME', 'projet');define('DB_PASSWORD', 'yZ5RufZ3LUVbpUPr');
+	define('DB_USERNAME', 'projet');
+	define('DB_PASSWORD', 'yZ5RufZ3LUVbpUPr');
+	//define('DB_USERNAME', '');define('DB_PASSWORD', '');
 	define('DB_NAME', 'registration');
 
 	// Connexion à la base de données MySQL 
