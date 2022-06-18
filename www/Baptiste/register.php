f2f7bf77d456c13d63a021e58d110b29627f1e78
RayaneB75 <rayane.belkhir@icloud.com>
2022-06-18 15:35:17 +0200

    Page d'inscription
    Adaptation iframe

--- www/Baptiste/register.php
+++ www/Baptiste/register.php
@@ -32,8 +32,6 @@ if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['password'])){
 }else{
 ?>
 <form class="box" action="" method="post">
-	<h1 class="box-logo box-title"><a href="">Retour acceuil</a></h1>
-    <h1 class="box-title">S'inscrire</h1>
 	<input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur" required />
     <input type="text" class="box-input" name="email" placeholder="Email" required />
     <input type="password" class="box-input" name="password" placeholder="Mot de passe" required />
