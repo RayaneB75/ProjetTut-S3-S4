ca7cdae6974a436a1a27b7d14dfc287c663418d7
RayaneB75 <rayane.belkhir@icloud.com>
2022-06-18 15:28:03 +0200

    Page de connexion
    18/06 - Adaptation pour iframe

--- www/Baptiste/login.php
+++ www/Baptiste/login.php
@@ -32,8 +32,6 @@ if (isset($_POST['username'])){
 }
 ?>
 <form class="box" action="" method="post" name="login">
-<h1 class="box-logo box-title"><a href="https://pt.rayane.space/Rayane/">Retour acceuil</a></h1>
-<h1 class="box-title">Connexion</h1>
 <input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur">
 <input type="password" class="box-input" name="password" placeholder="Mot de passe">
 <input type="submit" value="Connexion " name="submit" class="box-button">
