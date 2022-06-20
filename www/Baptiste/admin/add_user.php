d8c58c622b5a2ff05f5cdb032e9deb23cf46fd4d
Elpouletos <arbiolbaptiste@gmail.com>
2022-06-20 13:14:07 +0200

    fix
    ajout du top

--- www/Baptiste/admin/add_user.php
+++ www/Baptiste/admin/add_user.php
@@ -38,6 +38,7 @@ if (isset($_REQUEST['username'], $_REQUEST['email'], $_REQUEST['type'], $_REQUES
     <h1 class="box-title">Add user</h1>
 	<input type="text" class="box-input" name="username" placeholder="Nom d'utilisateur" required />
     <input type="text" class="box-input" name="email" placeholder="Email" required />
+	<p class="box-register"><a href="https://pt.rayane.space/Rayane/" target="_top">Retour à l'accueil</a></p>
 	<div class="input-group">
 			<select class="box-input" name="type" id="type" >
 				<option value="" disabled selected>Type</option>
