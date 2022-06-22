0367f5d7e3a6a89369f01e23a4955d0cd0faff7f
BELKHIR Rayane OBS/CSO <CMHL6904@orange.com>
2022-06-22 15:24:35 +0200

    Script backup container docker
    - Ajout nouveaux containers
    - Amélioration des dénominations

--- server-configuration/backup.sh
+++ server-configuration/backup.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-d=$(date %)
-# Backup des containers:
-for i in www openvpn ns portainer dicom mail
+rm /var/services/homes/rayane/backup/last/*
+echo
+echo `date`
+#Backup des containers:
+m=$(date | cut -d " " -f2)
+j=$(date | cut -d " " -f3)
+
+for i in www openvpn ns portainer orthanc emr
 do
-	echo "Backup du container $i en cours"
-	sudo docker commit -p $i backup-$i
-	echo "Sauvegarde dans le répertoire /var/services/homes/rayane/backup"
-	sudo docker save -o /var/services/homes/rayane/backup/backup-$i.tar backup-$i
+        echo "Backup du container $i en cours"
+        sudo docker commit -p $i backup-$i
+        mkdir -p /volume1/docker/rayane/backup/auto
+        echo "Sauvegarde dans le repertoire /volume1/docker/rayane/backup/auto"
+        sudo docker save -o /volume1/docker/rayane/backup/auto/$j-$m-$i.tar backup-$i
+        sudo chown rayane /volume1/docker/rayane/backup/auto/$j-$m-$i.tar
+        sudo chgrp users /volume1/docker/rayane/backup/auto/$j-$m-$i.tar
+        sudo chmod 744 /volume1/docker/rayane/backup/auto/$j-$m-$i.tar
+        sudo cp /volume1/docker/rayane/backup/auto/$j-$m-$i.tar /volume1/docker/rayane/backup/last/backup-$i.tar
 done
