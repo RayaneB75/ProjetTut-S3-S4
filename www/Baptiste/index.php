addb60d03c610f0eabeec706fecc84ead79487c1
Sourdoug <jassem.contact@gmail.com>
2022-06-23 18:44:30 +0200

    bouton vers l'espace perso
    ajout d'un bouton pour rediriger vers l'espace personnel

--- www/Baptiste/index.php
+++ www/Baptiste/index.php
@@ -15,14 +15,16 @@
 	</head>
 	<body>
 		<div class="success">
-		<h1>Bienvenue docteur <?php echo $_SESSION['username']; ?> !</h1>
-		<p>C'est votre espace utilisateur, vous pouvez visualiser vos fichier DICOM ici.</p>
-		<p>Pour accéder aux dossiers médicaux électroniques <a href=https://emr.rayane.space/openemr> cliquez ici </a></p>
-		</br>
-		<iframe title="dicomViewer" width="1280" height="720" src="https://dicom.rayane.space/">
-		</iframe>
-		</br>
-		<a href="logout.php">Déconnexion</a>
+			<h1>Bienvenue docteur <?php echo $_SESSION['username']; ?> !</h1>
+			<p>C'est votre espace utilisateur, vous pouvez visualiser vos fichier DICOM ici.</p>
+			<p>Pour accéder aux dossiers médicaux électroniques <a href=https://emr.rayane.space/openemr> cliquez ici </a></p>
+			<p>Pour acceder à votre espace personnel veuiller cliquer sur le bouton ci-dessous.</p>
+			<bouton onclick="windows.location.href='www\Programme_Jassem\profile.html';">Espace personnel</bouton>
+			</br>
+			<iframe title="dicomViewer" width="1280" height="720" src="https://dicom.rayane.space/">
+			</iframe>
+			</br>
+			<a href="logout.php">Déconnexion</a>
 		</div>
 	</body>
 <html>
