ceff675ef26210b23cbd92876814312b4ba3579a
Sourdoug <jassem.contact@gmail.com>
2022-06-24 00:34:39 +0200

    Update index.php

--- www/Baptiste/index.php
+++ www/Baptiste/index.php
@@ -34,7 +34,51 @@
 		</br>
 		<a href="logout.php">Déconnexion</a>
         <button id="Déconnexion">Déconnexion</button>
-			
+				
+					<!--Début du code pour le bouton vers la page de personnalisation de profile.-->
+					<p>Pour acceder à votre espace personnel veuiller cliquer sur le bouton ci-dessous.</p>
+			<bouton onclick="windows.location.href='www\Programme_Jassem\profile.html';">Espace personnel</bouton>
+					<style>
+					button {
+						display: inline-block;
+						background-color: #7b38d8;
+						border-radius: 10px;
+						border: 4px double #cccccc;
+						color: #eeeeee;
+						text-align: center;
+						font-size: 28px;
+						padding: 20px;
+						width: 200px;
+						transition: all 0.5s;
+						cursor: pointer;
+						margin: 5px;
+					}
+					button span {
+						cursor: pointer;
+						display: inline-block;
+						position: relative;
+						transition: 0.5s;
+					}
+					button span:after {
+						content: "\00bb";
+						position: absolute;
+						opacity: 0;
+						top: 0;
+						right: -20px;
+						transition: 0.5s;
+					}
+					button:hover {
+						background-color: #f7c2f9;
+					}
+					button:hover span {
+						padding-right: 25px;
+					}
+					button:hover span:after {
+						opacity: 1;
+						right: 0;
+					}
+				</style>
+<!--Fin du code pour le bouton vers la page de personnalisation de profile.-->
 		</div>
 	<body>
 <html>
